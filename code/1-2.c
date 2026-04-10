927d7742895e8ccf5dc3bc24971440baab76f37a
yosi475 <yosi475@naver.com>
2026-04-11 00:54:24 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-void lab_1_2() {
+int main() {
     int arr2[2][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}};
     int (*p)[4];
+
     for (int i = 0; i < 2; i++) {
-        p = arr2 + i;
+        p = arr2 + i; [cite: 92]
         for (int j = 0; j < 4; j++) {
-            printf("%d ", *(*p + j));
+            printf("%d\n", *(*p + j)); [cite: 97]
         }
     }
-    printf("\n");
+
+    return 0;
 }
